46cc0524d496f32d63900583a63f4dbb4dd5678b
Pedro Portella <contact@pedroportella.com>
2026-05-07 11:07:50 +1000

    chore: removed legacy js files (qgds)

--- packages/ui-library/src/theme/css/components/back_to_top/back-to-top.js
+++
@@ -1,8 +0,0 @@
-/**
- * Back To Top component logic attached to an init() function.
- * This logic is imported by the component's entry file (index.js) and ships with the component.
- */
-
-export default {
-  init() {},
-};
